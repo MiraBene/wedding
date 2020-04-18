b83bc1fbac5b863c797b8d0cce85784c3d494471
Benjamin Miraglio <benjamin@miraglio.fr>
2020-04-18 18:13:50 +0200

    Added first static files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return("My mobile web server!")
+    return render_template('index.html', style_path=url_for('static', filename='style.css'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
